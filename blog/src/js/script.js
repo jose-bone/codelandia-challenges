3f15a1ba176f9699f960e40107bea0025ead4c00
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-04-09 01:34:36 +0200

    Script e remoção da pasta de fontes

---
+++ blog/src/js/script.js
@@ -0,0 +1,25 @@
+//LOADING
+document.body.insertAdjacentHTML(
+  "beforeend",
+  `
+  <div id="loading">
+    <svg class="spinner" viewBox="0 0 50 50">
+      <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="3"></circle>
+    </svg>
+  </div>
+`
+);
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loading.remove();
+    app.removeAttribute("style");
+  }, 300);
+});
+
+// heart animation
+const card = document.querySelector("article");
+const heart = document.querySelector("#heart");
+
+card.addEventListener("click", function () {
+  heart.style.color = "red";
+});
